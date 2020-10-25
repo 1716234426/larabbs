36986ad1c07a556a56814e005578a9279b6cd1b7
yangzhiyi <1716234426@qq.com>
2020-10-26 00:00:58 +0800

    邮箱认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
     public function root()
     {
+//       dd(\Auth::user()->hasVerifiedEmail());
         return view('pages.root');
     }
 }
