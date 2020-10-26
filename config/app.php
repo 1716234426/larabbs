115b2ce96aca454e2b0fc8b29aea5b94fe7a543d
yangzhiyi <1716234426@qq.com>
2020-10-26 22:54:28 +0800

    修复 N+1 问题

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
